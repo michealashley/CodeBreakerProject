acb815de3319eac2395fd2405066a74682985e5d
Micheal Ashley <micheal.ashley@macefusion.com>
2017-08-15 15:02:58 -0400

    Adds setHiddenFields function

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,17 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer.value == '' || attempt.value == '') {
+        setHiddenFields();
+    }
 }
 
 //implement new functions here
+function setHiddenFields() {
+    attempt.value = 0;
+    answer.value = Math.floor(Math.random() * 10000).toString();
+    console.log(answer.value.length);
+    while (answer.value.length < 4) {
+        "0" += answer.value;
+    }
+}
